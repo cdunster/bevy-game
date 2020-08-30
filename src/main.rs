043c5d3929a3793ee23384e7eb51886b61b23038
cdunster <cdunster@users.noreply.github.com>
2020-08-30 21:36:57 +0200

    Load simple 2D sprite (bevy logo).

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,22 @@
 use bevy::prelude::*;
 
-struct Person;
-struct Name(String);
-struct GreetTimer(Timer);
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn((Person, Name("Laura Colbeck".to_string())))
-        .spawn((Person, Name("Callum Dunster".to_string())))
-        .spawn((Person, Name("Bailey van Colster".to_string())));
-}
-
-fn greet_people(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    mut query: Query<(&Person, &Name)>,
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    timer.0.tick(time.delta_seconds);
-    if timer.0.finished {
-        for (_person, name) in &mut query.iter() {
-            println!("Hello, {}!", name.0);
-        }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
+    let texture_handle = asset_server.load("assets/icon.png").unwrap();
+    commands
+        .spawn(Camera2dComponents::default())
+        .spawn(SpriteComponents {
+            material: materials.add(texture_handle.into()),
+            ..Default::default()
+        });
 }
 
 fn main() {
     App::build()
         .add_default_plugins()
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup.system())
         .run();
 }
